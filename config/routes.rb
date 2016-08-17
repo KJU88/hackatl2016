dc6fc65a6e25eb98a00effe924b147943144765d
Jonathan Liang <j.liang8888@gmail.com>
2016-08-17 19:08:32 +0000

    Installed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :contacts   #generating routes; can check with bundle exec rake routes
   get '/about' => 'pages#about'
   root 'pages#home'
